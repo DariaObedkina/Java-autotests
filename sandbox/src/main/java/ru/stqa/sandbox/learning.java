0f61dd36b753c4c018b90e23badee4a607fbd7e1
DariaObedkina <43385864+DariaObedkina@users.noreply.github.com>
2020-12-18 22:44:11 +0300

    Создан класс Point, в класс добавлена функция расчета расстояния между точками

--- sandbox/src/main/java/ru/stqa/sandbox/learning.java
+++ sandbox/src/main/java/ru/stqa/sandbox/learning.java
@@ -2,6 +2,11 @@ package ru.stqa.sandbox;
 
 public class Learning {
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        Point p1 = new Point(1,4);
+        Point p2 = new Point(-5,10);
+
+        System.out.println(p1.distance(p2));
     }
+
+
 }
