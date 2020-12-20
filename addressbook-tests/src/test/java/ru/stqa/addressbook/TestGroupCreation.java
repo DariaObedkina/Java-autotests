9562a2025ec5424f0f4dc5a0b91cf780648b5058
DariaObedkina <43385864+DariaObedkina@users.noreply.github.com>
2020-12-20 17:38:39 +0300

    произведен рефакторинг теста Group Creation

--- addressbook-tests/src/test/java/ru/stqa/addressbook/TestGroupCreation.java
+++ addressbook-tests/src/test/java/ru/stqa/addressbook/TestGroupCreation.java
@@ -1,13 +1,11 @@
 package ru.stqa.addressbook;
 
-import java.util.regex.Pattern;
 import java.util.concurrent.TimeUnit;
 import org.junit.*;
 import static org.junit.Assert.*;
-import static org.hamcrest.CoreMatchers.*;
+
 import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.Select;
 
 public class TestGroupCreation {
   private WebDriver driver;
@@ -17,21 +15,45 @@ public class TestGroupCreation {
   public void setUp() throws Exception {
     driver = new FirefoxDriver();
     driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    login("admin", "secret");
   }
 
   @Test
   public void testGroupCreation() throws Exception {
-    driver.get("https://localhost/addressbook/#");
-    driver.findElement(By.name("user")).sendKeys("admin");
-    driver.findElement(By.name("pass")).sendKeys("secret");
-    driver.findElement(By.xpath("//input[@value='Login']")).click();
-    driver.findElement(By.linkText("groups")).click();
-    driver.findElement(By.name("new")).click();
-    driver.findElement(By.name("group_name")).sendKeys("test");
-    driver.findElement(By.name("group_header")).sendKeys("testH");
-    driver.findElement(By.name("group_footer")).sendKeys("testF");
-    driver.findElement(By.name("submit")).click();
+    gotoGroupPage();
+    initGroupCreation();
+    fillGroupForm(new GroupData("test", "testH", "testF"));
+    submitGroupCreation();
+    returnToGroupPage();
+  }
+
+  private void returnToGroupPage() {
     driver.findElement(By.linkText("group page")).click();
+  }
+
+  private void submitGroupCreation() {
+    driver.findElement(By.name("submit")).click();
+  }
+
+  private void fillGroupForm(GroupData groupData) {
+    driver.findElement(By.name("group_name")).sendKeys(groupData.getName());
+    driver.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
+    driver.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
+  }
+
+  private void initGroupCreation() {
+    driver.findElement(By.name("new")).click();
+  }
+
+  private void login(String username, String password) {
+    driver.get("https://localhost/addressbook/#");
+    driver.findElement(By.name("user")).sendKeys(username);
+    driver.findElement(By.name("pass")).sendKeys(password);
+    driver.findElement(By.xpath("//input[@value='Login']")).click();
+  }
+
+  private void gotoGroupPage() {
+    driver.findElement(By.linkText("groups")).click();
   }
 
   @After
