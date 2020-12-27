1e0622e74a6f8813fdb5abd2d3b91aadf96af341
DariaObedkina <43385864+DariaObedkina@users.noreply.github.com>
2020-12-27 23:17:50 +0300

    добавлена возможность запускать тесты в разных браузерах

--- addressbook-tests/src/test/java/ru/stqa/addressbook/appmanager/ApplicationManager.java
+++ addressbook-tests/src/test/java/ru/stqa/addressbook/appmanager/ApplicationManager.java
@@ -2,24 +2,39 @@ package ru.stqa.addressbook.appmanager;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
 import static org.junit.Assert.fail;
 
 public class ApplicationManager {
+
     WebDriver driver;
     private SessionHelper sessionHelper;
     private NavigationHelper navigationHelper ;
     private GroupHelper groupHelper;
     private ContactHelper contactHelper;
     private StringBuffer verificationErrors = new StringBuffer();
+    private String browser;
+
+    public ApplicationManager(String browser) {
+        this.browser = browser;
+    }
 
     public void init() {
-        driver = new FirefoxDriver();
+        if(browser == BrowserType.FIREFOX) {
+            driver = new FirefoxDriver();
+        } else if (browser == BrowserType.CHROME) {
+            driver = new ChromeDriver();
+        } else if (browser == BrowserType.IE) {
+            driver = new InternetExplorerDriver();
+        }
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-        driver.get("https://localhost/addressbook/#");
+        driver.get("http://localhost/addressbook/"); //driver.get("https://localhost/addressbook/#");
         groupHelper = new GroupHelper(driver);
         navigationHelper = new NavigationHelper(driver);
         sessionHelper = new SessionHelper(driver);
