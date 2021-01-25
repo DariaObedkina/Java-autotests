1c1161a411f56167ae19490b9e2cf6240c7ec064
DariaObedkina <43385864+DariaObedkina@users.noreply.github.com>
2021-01-25 21:40:53 +0300

    Добавлено сравнение списков групп в TestGroupModification

--- addressbook-tests/src/test/java/ru/stqa/addressbook/tests/TestGroupModification.java
+++ addressbook-tests/src/test/java/ru/stqa/addressbook/tests/TestGroupModification.java
@@ -4,6 +4,7 @@ import org.junit.Assert;
 import org.junit.Test;
 import ru.stqa.addressbook.model.GroupData;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class TestGroupModification extends TestBase {
@@ -17,10 +18,16 @@ public class TestGroupModification extends TestBase {
         List<GroupData> before = app.getGroupHelper().getGroupList();
         app.getGroupHelper().selectGroup(before.size()-1);
         app.getGroupHelper().initGroupModification();
-        app.getGroupHelper().fillGroupForm(new GroupData("test1", "test", "test"));
+        GroupData group = new GroupData(before.get(before.size()-1).getId(),"test1", "test", "test");
+        app.getGroupHelper().fillGroupForm(group);
         app.getGroupHelper().submitGroupModification();
         app.getGroupHelper().returnToGroupPage();
         List<GroupData> after = app.getGroupHelper().getGroupList();
         Assert.assertEquals(before.size(),after.size());
+
+        before.remove(before.size()-1);
+        before.add(group);
+        Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
+
     }
 }
